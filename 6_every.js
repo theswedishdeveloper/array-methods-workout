4f712f7e56972e55d39fdcfff938bd74a0f2b533
Benjamin <=>
2020-12-07 16:15:54 +0100

    Solve 6

---
+++ 6_every.js
@@ -0,0 +1,10 @@
+// includes
+//
+// metoden söker igenom en array med en funktion som argument
+// den retunerar TRUE om alla element uppfyller funktionens villkor!
+
+const ages = [5, 10, 15, 20, 25, 30]
+
+const checkAdult = (age) => age >= 18
+
+console.log(ages.every(checkAdult)) //false
