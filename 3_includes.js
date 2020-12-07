f6541a7113029a96e489c245e8d7f742edd851a7
Benjamin <=>
2020-12-07 16:01:52 +0100

    Solve 3

---
+++ 3_includes.js
@@ -0,0 +1,10 @@
+// includes
+//
+// metoden söker igenom en array med ett visst argument
+// och retunerar TRUE om den finns i listan annars FALSE
+
+const things = ['car', 'house', 'castle', 'bridge', 'tree']
+
+console.log(things.includes("house")) // true
+
+console.log(things.includes('boat')) // false
