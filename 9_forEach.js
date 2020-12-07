d0856d0b6c3e0d614cc75967f4c8ac89f8e35bd1
Benjamin <=>
2020-12-07 16:26:15 +0100

    Solve 9

---
+++ 9_forEach.js
@@ -0,0 +1,8 @@
+// forEach
+//
+// metoden loppar igenom en array och retunerar varje element
+
+const numbers = [9, 25, 64, 256]
+numbers.forEach((number) => {
+    console.log(number) // t.ex. retunerar 9, 64, or 256
+})
