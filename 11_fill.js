65bc0acc7f11bb0ee18eeea00888ce3010bb26ca
Benjamin <=>
2020-12-07 16:36:08 +0100

    Solve 11

---
+++ 11_fill.js
@@ -0,0 +1,12 @@
+// fill
+//
+// metoden modiferar en array
+// tar ett argument och ändrar alla element i arrayen till värdet av argumentet
+
+let numbers = [1, 2, 3, 4, 5]
+
+console.log(numbers) //1, 2, 3, 4, 5
+
+numbers.fill(5);
+
+console.log(numbers) //5, 5, 5, 5, 5
