f2242439e1a64c99c6ce8fff453d218baefe6607
Benjamin <=>
2020-12-07 16:07:53 +0100

    Solve 4

---
+++ 4_push_pop.js
@@ -0,0 +1,22 @@
+// push
+//
+// metoden tar emot ett argument och lägger till det i en array
+
+const things = ['car', 'house', 'castle', 'bridge', 'tree']
+
+console.log(things) //car, house, castle, bridge, tree
+
+things.push("boat")
+
+console.log(things) //car, house, castle, bridge, tree, boat
+
+// pop
+//
+// metoden retunerar elementet med högst index
+// samt raderar element ur arrayen.
+
+const things2 = ['car', 'house', 'castle', 'bridge', 'tree']
+
+console.log(things2.pop()) //tree
+
+console.log(things2.pop()) //bridge
